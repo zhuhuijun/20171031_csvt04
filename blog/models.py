77a79515e22e16cb299c4b133de63c7b64c6dbe4
kngcbzdsn <zhuhjtcx@qq.com>
2017-10-31 08:15:49 +0800

    冒号

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Entry(models.Model)
+class Entry(models.Model):
     name = models.CharField(max_length=30)
 
     def __unicode__(self):
         return self.name
-    
+
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=30)
+    entry = models.ForeignKey(Entry)
+
+    def __unicode__(self):
+       return self.name
