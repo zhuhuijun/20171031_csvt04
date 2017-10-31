943f004a3db0094da0b751ee5a152846eb1e6027
kngcbzdsn <zhuhjtcx@qq.com>
2017-10-31 08:12:24 +0800

    manyto1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Entry(models.Model)
+    name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
+    
